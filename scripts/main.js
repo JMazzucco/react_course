55b65bfd15e8c86f58bb0780268732a6207d6962
John Mazzucco <John.Mazzucco@gmail.com>
2016-01-11 10:23:31 -0500

    require helpers and add dynamic default value for StorePicker input field

--- scripts/main.js
+++ scripts/main.js
@@ -8,6 +8,8 @@ var Navigation = ReactRouter.Navigation;
 
 var createBrowserHistory = require('history/lib/createBrowserHistory');
 
+var h = require('./helpers');
+
 /*
 	App
 */
@@ -60,7 +62,7 @@ var StorePicker = React.createClass({
 		return (
 			<form className="store-selector">
 				<h2>Please Enter A Store</h2>
-				<input type="text" ref="storeId" required />
+				<input type="text" ref="storeId" defaultValue={h.getFunName()} required />
 				<input type="submit"/>
 			</form>
 			)
