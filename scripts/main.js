daf0d221b0de90aa24a03f172ee557559de3bbff
John Mazzucco <John.Mazzucco@gmail.com>
2016-01-15 14:34:03 -0500

    display fish name, ordered amount, subtotal prices and total price under 'Your Order'

--- scripts/main.js
+++ scripts/main.js
@@ -27,14 +27,17 @@ var App = React.createClass({
 		this.state.fishes['fish-' + timestamp] = fish;
 		this.setState({ fishes : this.state.fishes });
 	},
-
+	addToOrder : function(key){
+		this.state.order[key] = this.state.order[key] + 1 || 1;
+		this.setState({order : this.state.order})
+	},
 	loadSamples : function() {
 		this.setState({
 			fishes : require('./sample-fishes')
 		})
 	},
 	renderFish : function(key){
-		return <Fish key={key} index={key} details={this.state.fishes[key]} />
+		return <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} />
 	},
 	render : function(){
 		return (
@@ -45,7 +48,7 @@ var App = React.createClass({
 						{Object.keys(this.state.fishes).map(this.renderFish)}
 					</ul>
 				</div>
-				<Order/>
+				<Order fishes={this.state.fishes} order={this.state.order}/>
 				<Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
 			</div>
 		)
@@ -58,8 +61,14 @@ var App = React.createClass({
 */
 
 var Fish = React.createClass({
+	onButtonClick : function(){
+		console.log("going to add a fish: ", this.props.index);
+		this.props.addToOrder(this.props.index);
+	},
 	render: function() {
 		var details = this.props.details;
+		var isAvailable = (details.status === 'available' ? true : false);
+		var buttonText = (isAvailable ? 'Add To Order' : 'Sold Out!');
 		return (
 			<li className="menu-fish">
 				<img src={details.image} alt={details.name} />
@@ -68,6 +77,7 @@ var Fish = React.createClass({
 					<span className="price">{h.formatPrice(details.price)}</span>
 				</h3>
 				<p>{details.desc}</p>
+				<button disabled={!isAvailable} onClick={this.onButtonClick}>{buttonText}</button>
 			</li>
 		)
 	}
@@ -122,9 +132,49 @@ var Header = React.createClass({
 });
 
 var Order = React.createClass({
-	render : function(){
+	renderOrder : function(key){
+		var fish = this.props.fishes[key];
+		var count = this.props.order[key];
+
+		if (!fish){
+			return <li key={key}>Sorry, fish no longer available</li>
+		}
+
 		return (
-			<p>Order</p>
+			<li>
+				{count}lbs
+				{fish.name}
+				<span className="price">{h.formatPrice(count * fish.price)}</span>
+			</li>
+
+			)
+	},
+	render : function(){
+		var orderIds = Object.keys(this.props.order);
+		var total = orderIds.reduce((prevTotal, key)=>{
+			var fish = this.props.fishes[key];
+			var count = this.props.order[key];
+			var isAvailable = fish && fish.status === 'available';
+
+			if(fish && isAvailable){
+				return prevTotal + (count * parseInt(fish.price) || 0)
+
+			}
+
+			return prevTotal;
+		}, 0);
+
+		return (
+			<div className="order-wrap">
+				<h2 className="order-title">Your Order</h2>
+				<ul className="order">
+					{orderIds.map(this.renderOrder)}
+					<li className="total">
+						<strong>Total:</strong>
+						{h.formatPrice(total)}
+					</li>
+				</ul>
+			</div>
 		)
 	}
 });
