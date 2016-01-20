6c641266030822d279cdaf82a4e8f13b9ddcea13
John Mazzucco <John.Mazzucco@gmail.com>
2016-01-20 11:39:51 -0500

    convert to ES6

--- scripts/components/Fish.js
+++ scripts/components/Fish.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import h from '../helpers';
+import autobind from 'autobind-decorator';
 /*
 	Fish
 	<Fish />
 */
+@autobind
+class Fish extends React.component{
 
-var Fish = React.createClass({
-	onButtonClick : function(){
+	onButtonClick(){
 		console.log("going to add a fish: ", this.props.index);
 		this.props.addToOrder(this.props.index);
-	},
-	render: function() {
+	}
+
+	render() {
 		var details = this.props.details;
 		var isAvailable = (details.status === 'available' ? true : false);
 		var buttonText = (isAvailable ? 'Add To Order' : 'Sold Out!');
@@ -26,6 +29,7 @@ var Fish = React.createClass({
 			</li>
 		)
 	}
-})
+
+}
 
 export default Fish;
