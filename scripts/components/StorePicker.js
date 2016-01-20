a7d3d1524c5c0edd1d1e6aaeab6a91e7a6f94ebd
John Mazzucco <John.Mazzucco@gmail.com>
2016-01-20 09:44:53 -0500

    convert StorePicker and NotFound components to ES6 class. Import react-mixin module for StorePicker

--- scripts/components/StorePicker.js
+++ scripts/components/StorePicker.js
@@ -2,20 +2,20 @@ import React from 'react';
 import { Navigation } from 'react-router';
 import { History } from 'react-router';
 import h from '../helpers';
+import reactMixin from 'react-mixin';
 // StorePicker
 // This will let us make <StorePicker/>
 
-var StorePicker = React.createClass({
-	mixins : [Navigation],
-	goToStore : function(event) {
+class StorePicker extends React.component{
+	goToStore(event) {
 		event.preventDefault();
 		console.log('Ya Submitted it!');
 		var storeId = this.refs.storeId.value;
 		this.history.pushState(null, '/store/' + storeId);
 		console.log(this);
-	},
+	}
 
-	render: function() {
+	render() {
 		return (
 			<form className="store-selector" onSubmit={this.goToStore}>
 				<h2>Please Enter A Store</h2>
@@ -24,6 +24,8 @@ var StorePicker = React.createClass({
 			</form>
 			)
 	}
-});
+};
+
+reactMixin.onClass(StorePicker, Navigation);
 
 export default StorePicker;
