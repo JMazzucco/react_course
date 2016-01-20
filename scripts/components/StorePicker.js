8824ee8a56f6648d64337e3842a900203d7aed29
John Mazzucco <John.Mazzucco@gmail.com>
2016-01-20 14:49:49 -0500

    Overwrote StorePicker  with the code from the course repo. Still getting an error.

--- scripts/components/StorePicker.js
+++ scripts/components/StorePicker.js
@@ -1,32 +1,34 @@
+/*
+  StorePicker
+  This will let us make <StorePicker/>
+*/
+
 import React from 'react';
-import { Navigation } from 'react-router';
 import { History } from 'react-router';
 import h from '../helpers';
 import reactMixin from 'react-mixin';
 import autobind from 'autobind-decorator';
-// StorePicker
-// This will let us make <StorePicker/>
 
 @autobind
-class StorePicker extends React.component{
-	goToStore(event) {
-		event.preventDefault();
-		console.log('Ya Submitted it!');
-		var storeId = this.refs.storeId.value;
-		this.history.pushState(null, '/store/' + storeId);
-		console.log(this);
-	}
+class StorePicker extends React.Component {
 
-	render() {
-		return (
-			<form className="store-selector" onSubmit={this.goToStore}>
-				<h2>Please Enter A Store</h2>
-				<input type="text" ref="storeId" defaultValue={h.getFunName()} required />
-				<input type="submit"/>
-			</form>
-			)
-	}
-};
+  goToStore(event) {
+    event.preventDefault();
+    // get the data from the input
+    var storeId = this.refs.storeId.value;
+    this.history.pushState(null, '/store/' + storeId);
+  }
+
+  render() {
+    return (
+      <form className="store-selector" onSubmit={this.goToStore}>
+        <h2>Please Enter A Store</h2>
+        <input type="text" ref="storeId" defaultValue={h.getFunName()} required />
+        <input type="Submit" />
+      </form>
+    )
+  }
+}
 
 reactMixin.onClass(StorePicker, History);
 
