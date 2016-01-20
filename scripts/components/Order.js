abb6683bac69e49a691cf14c8f92eab37d9be82c
John Mazzucco <John.Mazzucco@gmail.com>
2016-01-20 11:21:17 -0500

    convert to ES6

--- scripts/components/Order.js
+++ scripts/components/Order.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import CSSTransitionGroup from 'react-addons-css-transition-group';
 import h from '../helpers';
+import autobind from 'autobind-decorator';
 
-var Order = React.createClass({
-	renderOrder : function(key){
+@autobind
+class Order extends React.component{
+	renderOrder(key){
 		var fish = this.props.fishes[key];
 		var count = this.props.order[key];
 	    var removeButton = <button onClick={this.props.removeFromOrder.bind(null,key)}>&times;</button>
@@ -24,8 +26,9 @@ var Order = React.createClass({
 				</span>
 			</li>
 			)
-	},
-	render : function(){
+	}
+
+	render(){
 		var orderIds = Object.keys(this.props.order);
 		var total = orderIds.reduce((prevTotal, key)=>{
 			var fish = this.props.fishes[key];
@@ -57,14 +60,14 @@ var Order = React.createClass({
 				</CSSTransitionGroup>
 			</div>
 		)
-	},
+	}
+};
 
-	propTypes : {
+Order.propTypes = {
 		fishes : React.PropTypes.object.isRequired,
 		order : React.PropTypes.object.isRequired,
 		removeFromOrder : React.PropTypes.func.isRequired
-	}
-});
+}
 
 export default Order;
 
