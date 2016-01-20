255feb2e6eea3cb9f92f34d7c264617d2965c023
John Mazzucco <John.Mazzucco@gmail.com>
2016-01-20 10:28:51 -0500

    convert to ES6

--- scripts/components/Header.js
+++ scripts/components/Header.js
@@ -1,18 +1,18 @@
 import React from 'react';
 
-var Header = React.createClass({
-	render : function(){
+class Header extends React.component{
+	render(){
 		return (
 			<header className="top">
 				<h1>Catch of the Day</h1>
 				<h3 className="tagline">{this.props.tagline}</h3>
 			</header>
 		)
-	},
-
-	propTypes : {
-		tagline : React.PropTypes.string.isRequired
 	}
-});
+};
+
+Header.propTypes = {
+	tagline : React.PropTypes.string.isRequired
+}
 
 export default Header;
